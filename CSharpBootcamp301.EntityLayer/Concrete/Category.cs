623728fc9e8651be0e2cda36c3c5da095008a574
beste <beste@SERCAN>
2024-10-29 15:30:30 +0300

    Add project files.

---
+++ CSharpBootcamp301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBootcamp301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+/*
+ Field-Variable-Property
+ */
+
+//int x; direkt sınıfın içinde tanımlanırsa //Field
+
+//public int y { get; set; } get ve set metotları varsa //Property
+
+//void test()
+//{
+//    int z; bir metotun içine yazılıyorsa //Variable
+//}
---
+++ CSharpBootcamp301.EntityLayer/Concrete/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBootcamp301.EntityLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+    }
+}
