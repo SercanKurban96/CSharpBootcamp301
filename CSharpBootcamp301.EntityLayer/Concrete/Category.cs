b974b8d7cd6e4a9d4474284b15d4ca1b693e8d9c
beste <beste@SERCAN>
2024-11-24 15:43:23 +0300

    EntityLayer katmanı oluşturulup propertyler tanımlandı ve diğer katmanlar oluşturuldu.

---
+++ CSharpBootcamp301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBootcamp301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+/*
+ Field-Variable-Property
+ */
+
+/*
+ int x; --> Field
+ */
