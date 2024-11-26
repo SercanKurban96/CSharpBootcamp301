65573460edfe09496af084a1189b21734806ba65
beste <beste@SERCAN>
2024-11-26 23:38:03 +0300

    DataAccessLayer katmanında yer alan GenericRepository sınıfı, EntityState komutları ve EntityFramework sınıflara ait işlemler tamamlandı.

---
+++ CSharpBootcamp301.DataAccessLayer/EntityFramework/EfAdminDal.cs
@@ -0,0 +1,19 @@
+﻿using CSharpBootcamp301.DataAccessLayer.Abstract;
+using CSharpBootcamp301.DataAccessLayer.Repositories;
+using CSharpBootcamp301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBootcamp301.DataAccessLayer.EntityFramework
+{
+    public class EfAdminDal : GenericRepository<Admin>, IAdminDal
+    {
+    }
+}
+/*
+ * Ekle, Sil, Güncelle, Listele ve ID'ye Göre Getir (Entity'e özgü olmayan metot)
+ * İçinde a harfi geçmeyen kullanıcıları listele (Entity'e özgü olan metot)
+ */
---
+++ CSharpBootcamp301.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -0,0 +1,15 @@
+﻿using CSharpBootcamp301.DataAccessLayer.Abstract;
+using CSharpBootcamp301.DataAccessLayer.Repositories;
+using CSharpBootcamp301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBootcamp301.DataAccessLayer.EntityFramework
+{
+    public class EfProductDal : GenericRepository<Product>, IProductDal
+    {
+    }
+}
